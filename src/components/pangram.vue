caa896ac483d3accd2272a0082b1445adde718a2
Raphael Karanja <rkaranja@cytonn.com>
2018-05-07 14:31:26 +0300

    create a code that check whether a given block of code is a pagram

---
+++ src/components/pangram.vue
@@ -0,0 +1,62 @@
+<script>
+    export default  {
+        name: "Pangram",
+        data(){
+            return {
+                input: '',
+                status:'',
+                used_alphas: ''
+            }
+        },
+        watch:{
+            input: {
+                handler: function () {
+                    this.checkPangram();
+                }
+            }
+        },
+        methods: {
+            checkPangram() {
+//                c = String.fromCharCode(i);
+                let vm = this;
+                let alphabets = [];
+                let i = 'A'.charCodeAt(0);
+                let j = 'Z'.charCodeAt(0);
+
+                for (; i <= j; ++i) {
+                    alphabets.push(String.fromCharCode(i));
+                }
+                let s = this.input.replace(' ','').split('')
+                let state = 0;
+
+
+            let used_alphas = s.filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('');
+            this.used_alphas = used_alphas;
+
+                if (used_alphas.length >= 26) {
+                    this.status = 'We have ourselves a pangram'
+                } else {
+                    this.status = 'Data not enough to qualify a pangram';
+                }
+
+            }
+
+
+
+        }
+    }
+</script>
+<template>
+    <div>
+        <form >
+            <input type="text" v-model="input">
+
+            <h5>{{status}}</h5>
+            <p v-show="used_alphas != '' ">Used characters are: {{used_alphas}}</p>
+        </form>
+    </div>
+</template>
+
+<style>
+
+</style>
